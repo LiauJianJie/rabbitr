531d715d4e057d9e066f950b64893f8399e6efd4
Liau Jian Jie <contact.jianjie@gmail.com>
2015-04-09 01:31:10 +0800

    Added restrictions to usernames

---
+++ accounts.js
@@ -0,0 +1,18 @@
+if (Meteor.isClient) {
+  Meteor.subscribe("usernames");
+}
+
+if (Meteor.isServer) {
+  Meteor.publish("usernames", function() {
+    return Meteor.users.find({});
+  });
+
+  Accounts.validateNewUser(function (user) {
+    if (!user.username || user.username.length < 3)
+      throw new Meteor.Error(403, "Username must have at least 3 characters");
+    else if (!user.username.match(/^([A-Za-z]|[0-9]|_)+$/))
+      throw new Meteor.Error(403, "Username can only contain alphanumeric \
+                                   characters and underscores.");
+    return true;
+  });
+}
--- usernames.js
+++
@@ -1,9 +0,0 @@
-if (Meteor.isClient) {
-  Meteor.subscribe("usernames");
-}
-
-if (Meteor.isServer) {
-  Meteor.publish("usernames", function() {
-    return Meteor.users.find({});
-  });
-}
