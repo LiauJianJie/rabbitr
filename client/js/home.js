b328c44cac4546697b9a56eff782893729ad08ad
Liau Jian Jie <contact.jianjie@gmail.com>
2015-04-09 23:25:36 +0800

    Limited loading to 25 posts increment and added "load more posts" button

--- client/js/home.js
+++ client/js/home.js
@@ -1,7 +1,53 @@
 if (Meteor.isClient) {
+
+  var checkAndUpdatePost = function() {
+    var removeLinebreaksAndAutosize = function() {
+      var composeText = $("#tweet-compose-field").val();
+      composeText = composeText.replace(/(\r\n|\n|\r)/gm," ");
+      $("#tweet-compose-field").val(composeText);
+      $("#tweet-compose-field").autosize();
+    };
+
+    removeLinebreaksAndAutosize();
+    var charactersRemaining = 140-event.target.value.length;
+    if (charactersRemaining > 1 || charactersRemaining === 0)
+      $("#tweet-compose-charcount").html(charactersRemaining +
+                                         " characters remaining...");
+    else if (charactersRemaining === 1)
+      $("#tweet-compose-charcount").html("1 character remaining...");
+    else if (charactersRemaining < -1)
+      $("#tweet-compose-charcount").html(-charactersRemaining +
+                                         " characters too many...");
+    else
+      $("#tweet-compose-charcount").html("1 character too many...");
+
+    if (charactersRemaining < 0)
+    {
+      $("#tweet-compose-fieldgroup").addClass("has-error");
+      $("#tweet-compose-charcount").addClass("text-danger");
+      $("#tweet-compose-charcount").removeClass("text-muted");
+      $("#tweet-compose-submit").attr("disabled","disabled");
+    }
+    else if (charactersRemaining === 140)
+      $("#tweet-compose-submit").attr("disabled","disabled");
+    else
+    {
+      $("#tweet-compose-fieldgroup").removeClass("has-error");
+      $("#tweet-compose-charcount").removeClass("text-danger");
+      $("#tweet-compose-charcount").addClass("text-muted");
+      $("#tweet-compose-submit").removeAttr("disabled","disabled");
+    }
+  };
+
+  var loadTweet = function() {
+    var ITEMS_INCREMENT = 25;
+    Session.set("tweetsLimit",
+      Session.get("tweetsLimit") + ITEMS_INCREMENT);
+  };
+
   Template.home.helpers({
     tweets: function() {
-      return Tweets.find({}, {sort: {createdAt: -1},limit: 20});
+      return Tweets.find({}, {sort: {createdAt: -1}, limit:Session.get("tweetsLimit")});
     },
     tweetCount: function() {
       return Counts.get("tweetCounter");
@@ -11,6 +57,11 @@ if (Meteor.isClient) {
     },
     userCount: function() {
       return Counts.get("accountCounter");
+    },
+    moreResults: function() {
+        // If, once the subscription is ready, we have less rows than we
+        // asked for, we've got all the rows in the collection.
+        return !(Tweets.find().count() < Session.get("tweetsLimit"));
     }
   });
 
@@ -21,42 +72,24 @@ if (Meteor.isClient) {
       {
         Meteor.call("addTweet", text);
         event.target.text.value = "";
+        $("#tweet-compose-charcount").html("140 characters remaining...");
+        $("#tweet-compose-fieldgroup").removeClass("has-error");
+        $("#tweet-compose-charcount").removeClass("text-danger");
+        $("#tweet-compose-charcount").addClass("text-muted");
+        $("#tweet-compose-submit").attr("disabled","disabled");
       }
       return false;
     },
     "keyup #tweet-compose-field, \
      keydown #tweet-compose-field, \
      keypress #tweet-compose-field": function (event) {
-      $("#tweet-compose-field").autosize();
-      var charactersRemaining = 140-event.target.value.length;
-      if (charactersRemaining > 1 || charactersRemaining === 0)
-        $("#tweet-compose-charcount").html(charactersRemaining +
-                                           " characters remaining...");
-      else if (charactersRemaining === 1)
-        $("#tweet-compose-charcount").html("1 character remaining...");
-      else if (charactersRemaining < -1)
-        $("#tweet-compose-charcount").html(-charactersRemaining +
-                                           " characters too many...");
-      else
-        $("#tweet-compose-charcount").html("1 character too many...");
-
-      if (charactersRemaining < 0)
-      {
-        $("#tweet-compose-fieldgroup").addClass("has-error");
-        $("#tweet-compose-charcount").addClass("text-danger");
-        $("#tweet-compose-charcount").removeClass("text-muted");
-        $("#tweet-compose-submit").attr("disabled","disabled");
-      }
-      else if (charactersRemaining === 140)
-        $("#tweet-compose-submit").attr("disabled","disabled");
-      else
-      {
-        $("#tweet-compose-fieldgroup").removeClass("has-error");
-        $("#tweet-compose-charcount").removeClass("text-danger");
-        $("#tweet-compose-charcount").addClass("text-muted");
-        $("#tweet-compose-submit").removeAttr("disabled","disabled");
-      }
-    }
+       if(event.which == '13') {
+         $("#tweet-compose-submit").click();
+         return false;
+       }
+      checkAndUpdatePost();
+    },
+    "click #loadpost-button": loadTweet
   });
 
   Template.home.created = function() {
